d6ec5ea272fbde5f540e0d624776b2f34baec42b
yash-2115 <mailforyash04@gmail.com>
2022-02-07 13:23:45 +0530

    Data render in blog home page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Blog, BlogComment
 
 # Create your views here.
 def blog_home(request):
-    return render(request, "main/blog_home.html")
+    all_blogs = Blog.objects.all()
+    context = {
+        'blogs': all_blogs
+    }
+    return render(request, "main/blog_home.html", context)
 
 def blog_detail(request):
     return render(request, "main/blog_detail.html")
