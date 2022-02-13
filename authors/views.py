52a090258b6d9760ec734d3b50637cccae51c95e
Yash Patel <mailforyash04@gmail.com>
2022-02-13 18:07:01 +0530

    User creation form

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def signUp(request):
-    return render(request, "authors/register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        messages.success(request, "Your account is created successfully")
+        return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, "authors/register.html", {'form': form})
 
 def logIn(request):
     return render(request, "authors/login.html")
