77920b2b110f36e63039b2129e51f1df621264ad
Yash Patel <mailforyash04@gmail.com>
2022-03-01 14:46:26 +0530

    Added font awesome

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main',
     'authors',
+    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
